a3cbe3412c3e8a8db20f59c33ebcc09b2d081959
Ryan <rmthomas1998@gmail.com>
2024-05-01 16:33:08 -0400

    delete all files from main

--- backend/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// import all routes here and then import this file in server.js
-const userRouter = require('./user');
-router.use('/user', userRouter)
-
-module.exports = router;
