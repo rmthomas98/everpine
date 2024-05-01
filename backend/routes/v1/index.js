88eebbd330952c95269f20937e443884fd063f64
Ryan <rmthomas1998@gmail.com>
2024-05-01 16:29:16 -0400

    project init

---
+++ backend/routes/v1/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// import all routes here and then import this file in server.js
+const userRouter = require('./user');
+router.use('/user', userRouter)
+
+module.exports = router;
