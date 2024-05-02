4d7957aa1cd88e82bbc5b08298fc7b8113938b17
rmthomas98 <rmthomas1998@gmail.com>
2024-05-02 06:26:18 -0400

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const apiV1Router = require('./routes/v1')
-app.use('/api/v1', apiV1Router)
+const apiV1Router = require("./routes/v1");
+app.use("/v1", apiV1Router);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
